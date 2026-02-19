2f1d32e89e94b43f49bf6531da586ea9eff6a33e
Grace Desrochers <gracedes@bu.edu>
2026-02-18 19:56:59 -0500

    added a (not yet functioning) search bar

--- src/components/PuyoNexus.tsx
+++ src/components/PuyoNexus.tsx
@@ -21,21 +21,39 @@ import {Games} from "../interfaces/Games.ts";
     justify-content: center;
     width: 30%;
     padding: 2%;
-    margin: 1% auto;
-    border-width: 3px;
-    border-style: solid;
+    margin: 1%;
+    border: 3px rgb(98, 180, 225) solid;
     border-radius: 15px;
     font: italic small-caps bold calc(2px + 1vw) Arial;
     text-align: center;
      
        // TODO: change later
-     border-color: rgb(98, 180, 225);
      box-shadow: 0 0 10px rgb(98, 180, 225);
      background-color: ${(props) => (`rgba(98, 180, 255, ${(Games[props.lastGame] - Games[props.firstGame]) / (1.75 * (2026 - 1989))})`)};
      
     &:hover {
         box-shadow: 0 0 15px rgb(98, 180, 225);
     }
+ `;
+
+ const SearchDiv = styled.div`
+     text-align: center;
+     margin-top: 1vh;
+     font: italic small-caps bold calc(2px + 1vw) Arial;
+     
+     & input {
+         border: 3px rgb(98, 180, 225) solid;
+         border-radius: 15px;
+         box-shadow: 0 0 10px rgb(98, 180, 225);
+         width: 96%;
+         margin: 2vh 2%;
+         padding: 1%;
+         
+         &:focus {
+             box-shadow: 0 0 15px rgb(98, 180, 225);
+             outline: none;
+         }
+     }
  `;
 
 export default function PuyoNexusContent(){
@@ -50,21 +68,28 @@ export default function PuyoNexusContent(){
         fetchData().then(()=>console.log("okay")).catch((e) => console.log(e));
     }, [characters.length]);
 
+    // TODO: input label
     return (
-        <CharsContainer>
-            {
-                characters.map((char: Character) =>
-                    <CharDiv firstGame={char.firstAppear !== undefined ? char.firstAppear.split(",")[0] : char.firstAppear} lastGame={char.lastAppear !== undefined ? char.lastAppear.split(",")[0] : char.lastAppear}>
-                        <h1>{char.name}</h1>
-                        <h2>({char.nameJP.unicode})</h2>
-                        <p>Gender: {char.gender}</p>
-                        <p>{char.birthday !== undefined ? `Birthday: ${char.birthday}` : ``}</p>
-                        <p>{char.firstAppear !== undefined ? `First Appearance: ${char.firstAppear.split(",")[0]}` : ``}</p>
-                        <p>{char.lastAppear !== undefined ? `Last Appearance: ${char.lastAppear.split(",")[0]}` : ``}</p>
-                    </CharDiv>
-                )
-            }
-        </CharsContainer>
+        <>
+            <SearchDiv>
+                <h1>Puyo Puyo Characters</h1>
+                <input type={"text"} id={"charSearch"} placeholder={"Search..."}/>
+            </SearchDiv>
+            <CharsContainer>
+                {
+                    characters.map((char: Character) =>
+                        <CharDiv firstGame={char.firstAppear !== undefined ? char.firstAppear.split(",")[0] : char.firstAppear} lastGame={char.lastAppear !== undefined ? char.lastAppear.split(",")[0] : char.lastAppear}>
+                            <h1>{char.name}</h1>
+                            <h2>({char.nameJP.unicode})</h2>
+                            <p>Gender: {char.gender}</p>
+                            <p>{char.birthday !== undefined ? `Birthday: ${char.birthday}` : ``}</p>
+                            <p>{char.firstAppear !== undefined ? `First Appearance: ${char.firstAppear.split(",")[0]}` : ``}</p>
+                            <p>{char.lastAppear !== undefined ? `Last Appearance: ${char.lastAppear.split(",")[0]}` : ``}</p>
+                        </CharDiv>
+                    )
+                }
+            </CharsContainer>
+        </>
     );
 }
 // TODO: parse appearances for sonic
